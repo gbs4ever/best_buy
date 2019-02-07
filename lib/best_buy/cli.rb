f8860fbef0a6df0defcf7bd4a8de8828df5e62e3
gbs4ever <gbs4ever2@gmail.com>
2019-02-07 18:16:43 -0500

    bonus and moreinfo cli borken

--- lib/best_buy/cli.rb
+++ lib/best_buy/cli.rb
@@ -2,27 +2,33 @@ class Controller
     def call
 
         puts "Welcome to Best Buy Deal of the day."
+        puts ""
         puts "To see today's deal enter 'deal'."
         puts "what would you like to do"
+        puts""
         puts "To quit type exit. "
        
- #remove white space in menu
- #bug 1 exit works but still puts out invaild command
-        input = nil
+            input = nil
         while input !=  "exit"
         input= gets.strip.downcase
             case input
            when "deal"
             message
             file=deal
+            sleep 1.0
             puts "To see Bonus Deals of the Day press 'bonus'"
-           when"bonus"
+            when"bonus"
+            message   
             bonus_deals
             when "more info"
-                info#(file)
+                message
+                info(file)
+            when "exit"
+                puts "Good bye have a good day"
             else
-            puts "Invaild command please type a vaild command."
-            #puts "what would you like to do, press current deals or exit."
+                puts "Invaild command please type a vaild command."
+                puts ""
+                puts "what would you like to do, press current deals or exit."
             end
 
         end
@@ -33,39 +39,47 @@ class Controller
                 price=file.price.split(" ").map {|x| x[/\d+/]}.compact.join("").to_i
                 puts "Today's deals: #{file.name} $#{price}" 
                     sleep 0.9
-                puts "For more information please type yes"
+                    puts "These deals end at midnight tonight!!"
+                puts "For more information please type more info"
             else
                 puts "There are no deals today see www.bestbuy.com for great coupons"
-                file
+                file # is the object
             end
         end
         
         
-        #still broken
-        def info(file=nil)# is the object
-            #Scraper.more_info (file.url=nil)
+     
+        def info(file)
+            Scraper.more_info (file)
             puts  <<-DOC
             Moreinfo
             ================
-            1.  #{item.long_name} 
+            1. #{item.long_name} 
             
             2.#{item.long_price}
            
             3. #{item.info}
-            4.Make sure to buy today you saved #{item.saved}
+            4.Great you saved #{item.saved}
             --------------------------------
             Thank you for shopping at Best Buy
             DOC
         end
-        def  bonus_deals
-            puts "bonus deals lol"
-        end
-            def message
+                def  bonus_deals
+                bonus=Scraper.bonus_deals
+                binding.pry
+            # puts "Today's bonus deals: #{file.name} $#{file.price}"
+                end
+                    
+        
+        def message
                 puts "Hold on one second we are searching the web ..........................."
+                puts ""
+                puts ""
+                sleep 0.8
                 puts "Sorry for the wait , your patience is duly noted !" 
             end
 
-         #  Item.all.each.with_index  do |item,i| when i swirch do many deals
+         
 
 
 
--- lib/best_buy/scraper.rb
+++ lib/best_buy/scraper.rb
@@ -16,40 +16,46 @@ end
 
 #scrapper 2 
     def self.deal_of_the_day(deals_url)
-   # doc = Nokogiri::HTML(open("https://www.bestbuy.com/site/misc/deal-of-the-day/pcmcat248000050016.c?id=pcmcat248000050016"))
-    doc1 = Nokogiri::HTML(open(deals_url))
-   doc =doc1.css("div.offer-column.oneUp.col-xs-12")
+   
+    @@doc1 = Nokogiri::HTML(open(deals_url))
+   doc =@@doc1.css("div.offer-column.oneUp.col-xs-12")
 
     name=doc.css(".offer-link  a").text # name of item
     url = "https://www.bestbuy.com"
     url +=doc.css(".offer-link  a").attribute("href").value # link file
-    binding.pry
-    price=doc.css("span.sr-only")[0].text if doc.css("span.sr-only") # price 
-    sale = doc.css("div.pricing-price__sale-message").last.text #on sale should return true 
+    price=doc.css("span.sr-only").text if doc.css("span.sr-only") # price 
+    sale = doc.css("div.pricing-price__sale-message").text #on sale should return true 
     message= doc.css("div .add-to-cart-button").text
     Item.new(name, price, url, sale, message)
   
     
     end
-    #scrapper additional deals 
-            
-    #name=doc.css(" list-header-text v-fw-medium").text # to check that it is bonus deals
-    #file= doc.css(".col-xs-10 product")
 
+    def self.bonus_deals
+        list_main=@@doc1.css("div.widget-list-offer-container")
+        list= list_main.css(".row")
+        list.each.with_index  do |item,i| item
+            #scrapped data
+        name=item.css("h3").text
+        url = "https://www.bestbuy.com"
+       url+=item.css("h3 a").attribute("href").value # link file
+       price_data=item.css(".col-xs-4").first.text
+       price=price_data.split(" ")[0]
+       Item.new(name, price, url)
+        end
+    end
 
             
-#scrapper 3
+
 
     def self.more_info(item) # item should be object
         
-        #doc = Nokogiri::HTML(open(item))
-        item.saved =#doc.css("div .pricing-price__savings").text #amont saved
-        item.info =#doc.css("div #long-description").text #more info
-        item.long_name =#doc.css("h1").text    # name
-        item.long_price = "123"#doc.css("span.sr-only")[0].text # price
-    
-      
-    end
+        doc = Nokogiri::HTML(open(item.url))
+        item.saved =doc.css("div .pricing-price__savings").text #amont saved
+        item.info =doc.css("div #long-description").text #more info
+        item.long_name =doc.css("h1").text    # name
+        item.long_price =doc.css("span.sr-only")[0].text # price
+        end
 
 
     
